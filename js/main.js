c4c6a811e22580a4ba93492ff3408d54e90414d9
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2021-06-03 11:44:18 +0300

    2 функции
    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 const randomizerIntegerInRange = (min, max) => {
-  if (max-min<1 || min<0){ //max-min<1 проверка того, чтобы хотя бы одно целое число было в диапозоне//
-    return 'Ошибка диапазона';
+  if (Math.trunc(min) >= Math.trunc(max) || min <0){ //проверка того, чтобы хотя бы одно целое число было в диапозоне//
+    return Error('Ошибка диапазона');
   }
-  const Random = min + Math.random()*(max-min);
-  return Math.floor(Random);
+  const random = min + Math.random() * (max-min);
+  if (Math.round(random) > max){  //Проверка за выход из диапозона, например при (2.3,3.8) math.round мог выдавать как 2, так и 4//
+    return Math.floor(max);
+  } else if (Math.round(random) < min){
+    return Math.ceil(min);
+  }
+  return Math.round(random);
 };
 randomizerIntegerInRange();
 
 const randomizerFloatInRange = (min,max,decimalPoint) =>{
-  if (min>=max || min<0 ){
-    return 'Ошибка диапазона';
+  if (min >= max || min < 0){
+    return Error('Ошибка диапазона');
   }
-  const Random = min + Math.random()*(max-min);
-  return (Math.floor(10**decimalPoint*Random))/(10**decimalPoint);
+  const random = min + Math.random() * (max - min);
+  return (Math.floor(10**decimalPoint * random)) / (10**decimalPoint);
 
 };
 randomizerFloatInRange();
