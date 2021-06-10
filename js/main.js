3237d70a250469279bc8ceb8153b309ac1541b44
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2021-06-10 18:30:14 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,19 +1,72 @@
-const randomizerIntegerInRange = (min, max) => {
-  if (max === undefined){
-    return randomizerIntegerInRange(1, min);
-  }
-  if (Math.ceil(min)>Math.floor(max)){
-    return NaN;
-  }
-  return  Math.floor(Math.ceil(min) + Math.random() * (Math.floor(max) - Math.ceil(min) + 1));
-};
-randomizerIntegerInRange();
 
-const randomizerFloatInRange = (min,max,decimalPoint) =>{
-  if (min >= max || min < 0){
-    return NaN;
-  }
-  return Math.floor(randomizerIntegerInRange(min * 10 ** decimalPoint, max * 10 ** decimalPoint)) / (10 ** decimalPoint);
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
 
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+}
+const TYPE = ['palace', 'flat', 'house', 'bungalow','hotel'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const IN_OUT_TIME = ['12:00', '13:00','14:00'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const avatarRandomizer = () =>{
+  const random = getRandomPositiveInteger(0,10);
+  if (random===0 || random===10){
+    return random;
+  }
+  return '0'+ random;
 };
-randomizerFloatInRange();
+const typeRandomizer = () => {
+  return TYPE[getRandomPositiveInteger(0,4)];
+};
+const checkInOutRandomizer = () => {
+  return IN_OUT_TIME[getRandomPositiveInteger(0,2)];
+};
+const featuresRandomizer = () => {
+  return FEATURES.slice(getRandomPositiveInteger(0,5));
+};
+const photosRandomizer = () => {
+  return PHOTOS.slice(getRandomPositiveInteger(0,2));
+};
+
+const createAuthor = () => {
+  return {
+    avatar : 'mg/avatars/user'+avatarRandomizer()+'.png',
+  };
+};
+const createOffer = () => {
+  return {
+    title : 'Заголовок '+getRandomPositiveInteger(0,10),
+    addres : getRandomPositiveFloat(0,90,6)+', '+getRandomPositiveFloat(0,90,6),
+    price : getRandomPositiveInteger(0,100000),
+    rooms : getRandomPositiveInteger(0,100),
+    type : typeRandomizer(),
+    quests : getRandomPositiveInteger(0,200),
+    checkin : checkInOutRandomizer(),
+    checkout: checkInOutRandomizer(),
+    features: featuresRandomizer(),
+    description: 'Описание '+getRandomPositiveInteger(0,10),
+    photos: photosRandomizer(),
+  };
+};
+const createLocation = () => {
+  return {
+    lat : getRandomPositiveFloat(35.65000,35.70000,5),
+    lng : getRandomPositiveFloat(139.70000,139.80000,5),
+  };
+};
+const createCard = () => {
+  return {
+    author:createAuthor(),
+    offer:createOffer(),
+    location: createLocation(),
+  };
+};
+const cardsOffer = new Array(10).fill(null).map(() => {return createCard()});
